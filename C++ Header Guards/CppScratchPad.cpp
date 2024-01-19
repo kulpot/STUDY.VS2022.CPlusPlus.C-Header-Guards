3c482f1e4c37eb079803c41cdbcb8bae7fabc9e2
Kulpot <kulpotwahu@gmail.com>
2024-01-19 18:50:00 +0800

    ...

---
+++ C++ Header Guards/CppScratchPad.cpp
@@ -0,0 +1,26 @@
+// 3 Steps Preprocessor process
+// 1. Preprocessor, 2. Compiler, 3. Linker
+#include "AnotherCppFile.h" // Preprocessor ---- Header file ------
+#include <iostream>
+using std::cout;
+using std::endl;
+
+// -------------------- C++ Header Files -------------------------------------------------------
+//ref link:https://www.youtube.com/watch?v=Wa6irrxFG88&list=PLRwVmtr-pp05Bl0j6lwXd8tU754nUEB5P
+
+// Code Files - has .cpp extensions
+// Header Files - has .h extensions and some .cpp extensions
+
+//// -----Declarations----- should be in header file
+//void foo();
+//void foo1();
+//void foo2();
+//void foo3();
+//void foo4();
+
+void main()
+{
+	foo();
+	foo2();
+	foo4();
+}
